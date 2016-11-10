e513162c66151583af4df346fb6587273d2a3f04
José Caique Oliveira da Silva | Stone <jcosilva@stone.com.br>
2016-11-10 20:40:25 -0200

    partial login twitter

--- app/src/main/java/cade_a_nota/bigmini/com/br/cade_a_nota/Application.java
+++ app/src/main/java/cade_a_nota/bigmini/com/br/cade_a_nota/Application.java
@@ -1,15 +1,25 @@
 package cade_a_nota.bigmini.com.br.cade_a_nota;
 
 import com.facebook.FacebookSdk;
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Kanda on 08/11/2016.
  */
 
 public class Application extends android.app.Application {
+
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "gq2uCGSmIBI6Aptyd3VHh3Xv7";
+    private static final String TWITTER_SECRET = "SZMzuru3KpACS38EZ7VqyFTisBri55njauz9kVtO642ZkcS4GZ";
+
     @Override
     public void onCreate() {
         super.onCreate();
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
 
     }
 }
