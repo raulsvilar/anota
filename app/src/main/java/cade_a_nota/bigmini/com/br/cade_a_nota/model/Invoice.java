827f52327817642345fb2b25d00b68d7d574b25c
Allyson Rodrigues <allyson.rodrigues@paggcerto.com.br>
2016-11-16 22:07:33 -0300

    nota fiscal

---
+++ app/src/main/java/cade_a_nota/bigmini/com/br/cade_a_nota/model/Invoice.java
@@ -0,0 +1,16 @@
+package cade_a_nota.bigmini.com.br.cade_a_nota.model;
+
+import java.util.Date;
+
+/**
+ * Created by Allyson on 15/11/2016.
+ */
+
+public class Invoice {
+
+    private String title;
+    private String url;
+    private Date date;
+
+
+}
--- app/src/main/java/cade_a_nota/bigmini/com/br/cade_a_nota/views/activities/MainActivity.java
+++ app/src/main/java/cade_a_nota/bigmini/com/br/cade_a_nota/views/activities/MainActivity.java
@@ -2,14 +2,39 @@ package cade_a_nota.bigmini.com.br.cade_a_nota.views.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.Result;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import cade_a_nota.bigmini.com.br.cade_a_nota.R;
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler{
+    private ZXingScannerView mScannerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void startScanner(View view) {
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+        List<BarcodeFormat> formats = new ArrayList<>();
+        formats.add(BarcodeFormat.QR_CODE);
+        mScannerView.setFormats(formats);
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void handleResult(Result result) {
+        Log.e("QRCODE", result.getText());
     }
 }
